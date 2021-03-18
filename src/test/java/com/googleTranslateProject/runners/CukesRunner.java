ab59e7c509e645ca716f7ea2dd07f7b274c18825
Muhammet Hamdi Özdil <50085757+HamdiOzdil@users.noreply.github.com>
2021-03-18 15:17:38 +0100

    Some fixes in CukesRunner

--- src/test/java/com/googleTranslateProject/runners/CukesRunner.java
+++ src/test/java/com/googleTranslateProject/runners/CukesRunner.java
@@ -10,9 +10,9 @@ import org.junit.runner.RunWith;
                 "html:target/default-html-reports",
                 "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
-        glue = "com/group7Project/step_definitions",
+        glue = "com/googleTranslateProject/step_definitions",
         dryRun = false,
-        tags = "@SearchProject"
+        tags = "@example"
 )
 public class CukesRunner {
 
