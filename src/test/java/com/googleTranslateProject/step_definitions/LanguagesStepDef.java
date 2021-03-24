4299b9aa0d12986951a65ea03473a718312546bb
ismailsinansahin <ismailsinansahin@gmail.com>
2021-03-24 17:02:43 +0300

    sahin

---
+++ src/test/java/com/googleTranslateProject/step_definitions/LanguagesStepDef.java
@@ -0,0 +1,24 @@
+package com.googleTranslateProject.step_definitions;
+
+import com.group8.pages.TranslatePage;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.List;
+
+public class LanguagesStepDef {
+
+    TranslatePage translatePage = new TranslatePage();
+
+    @When("user click on the source language dropdown button")
+    public void user_click_on_the_source_language_dropdown_button() {
+        translatePage.clickOnSourceLanguageDropdownButton();
+    }
+
+    @Then("user should see all languages")
+    public void user_should_see_all_languages(List<String> expectedLanguagesList) {
+        List<String> actualLanguagesList = translatePage.createActualLanguagesList();
+        translatePage.verifyLanguages(expectedLanguagesList,actualLanguagesList);
+    }
+
+}
