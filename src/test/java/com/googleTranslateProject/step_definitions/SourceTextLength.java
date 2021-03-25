f537e32cafe0f4ac7724f1d4676febc05245239f
bakyvelmammedov <bakyvelmammedov@gmail.com>
2021-03-25 15:30:01 +0400

    updated SourceTextLength.java -created getString method -asserted actual and expected text length

--- src/test/java/com/googleTranslateProject/step_definitions/SourceTextLength.java
+++ src/test/java/com/googleTranslateProject/step_definitions/SourceTextLength.java
@@ -6,27 +6,21 @@ import io.cucumber.java.en.*;
 
 public class SourceTextLength {
     TextLength textLength = new TextLength();
-    TranslatePage translatePage = new TranslatePage();
 
     @Given("input text to source Translate Box")
     public void input_text_to_source_Translate_Box() throws InterruptedException {
-        translatePage.sourceWordInputBox.sendKeys(textLength.generateRandomWords());
+        textLength.sendText();
         Thread.sleep(5000);
     }
 
     @When("calculate length of source text")
     public void calculate_length_of_source_text() throws InterruptedException {
-        String inputText = translatePage.sourceWordInputBox.getText();
-        System.out.println(translatePage.sourceWordInputBox.isDisplayed());
-        int lenghtOfText = inputText.length();
-        System.out.println("inputText = " + inputText);
-        System.out.println("lenghtoOfText = " + lenghtOfText);
-        Thread.sleep(3000);
+        textLength.calculateTextLength();
     }
 
     @Then("verify length of source text is matching number displayed")
-    public void verify_length_of_source_text_is_matching_number_displayed() {
-        System.out.println("length = " + textLength.letterCount.getText());
+    public void verify_length_of_source_text_is_matching_number_displayed() throws InterruptedException {
+        textLength.verifyTextLength();
 
     }
 
